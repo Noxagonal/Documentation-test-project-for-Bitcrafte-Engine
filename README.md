b6e4347685eb11fea37d9602fdfd9e8e2db15390
Niko Kauppi <Noxagonal@gmail.com>
2024-09-15 12:52:49 -0400

    Removed doxygen generated entries, too much work and output wasn't looking too nice.

--- README.md
+++ README.md
@@ -3,16 +3,12 @@
 
 ## Requirements for local dev
 - python 3.x
-- doxygen
 - mkdocs-material
-- mkdoxy
-- python-dotenv
 
 ### Getting started on local development.
 
 - Install python 3.
-- Install doxygen.
 - Install pip dependencies:
 ```
-pip install mkdocs-material mkdoxy python-dotenv
+pip install mkdocs-material
 ```
--- regenerate_mkdocs.py
+++ regenerate_mkdocs.py
@@ -1,27 +1,17 @@
-import os
 import logging
-from dotenv import load_dotenv
 
 template_file = "mkdocs_template.yml"
 output_file = "mkdocs.yml"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-# see if ENGINE_REPO_DIR is set, if not, load .env file
-engine_repo_dir = os.getenv('ENGINE_REPO_DIR')
-if engine_repo_dir is None:
-	load_dotenv()
-	engine_repo_dir = os.getenv('ENGINE_REPO_DIR')
-
-if engine_repo_dir is None:
-	raise Exception("ENGINE_REPO_DIR is not set anywhere")
-
 with open(template_file, "r") as file:
 	template_content = file.read()
 
-final_content = template_content.replace("{{ENGINE_REPO_DIR}}", engine_repo_dir)
+# We can automate the generation of mkdocs.yml content here as needed.
+final_content = template_content
 
 with open(output_file, "w") as file:
 	file.write(final_content)
 
-logging.info(f"Generated mkdocs.yml with ENGINE_REPO_DIR = {engine_repo_dir}")
+logging.info("Generated mkdocs.yml")
